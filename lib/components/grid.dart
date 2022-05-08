82c276f820f4a5cfbff77845b6100e1941ff6383
Hiebeler <emanuel.hiebeler@gmail.com>
2022-05-08 14:28:47 +0200

    changed Grid width to 8

--- lib/components/grid.dart
+++ lib/components/grid.dart
@@ -8,23 +8,23 @@ class Grid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
           color: Color.fromRGBO(59, 66, 79, 1.0),
           borderRadius: BorderRadius.all(Radius.circular(6.0))),
       child: GridView(
-        physics: NeverScrollableScrollPhysics(),
+        physics: const NeverScrollableScrollPhysics(),
         shrinkWrap: true,
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 10, crossAxisSpacing: 6, mainAxisSpacing: 6),
+            crossAxisCount: 8, crossAxisSpacing: 6, mainAxisSpacing: 6),
         children: [
           ...(gridList.map((e) {
             return (Container(
               decoration: BoxDecoration(
                 color: e == 1
-                    ? Color.fromRGBO(8, 181, 102, 1.0)
-                    : Color.fromRGBO(40, 45, 54, 1.0),
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                    ? Theme.of(context).primaryColor
+                    : const Color.fromRGBO(40, 45, 54, 1.0),
+                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
               ),
             ));
           }))
