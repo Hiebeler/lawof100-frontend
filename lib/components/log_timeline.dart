6f12c380d90694ed994dc8f8f11c5d3b57d0c606
Hiebeler <emanuel.hiebeler@gmail.com>
2022-05-13 15:54:16 +0200

    timeline with done and fitted box

--- lib/components/log_timeline.dart
+++ lib/components/log_timeline.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 class LogTimeline extends StatelessWidget {
   Map entry;
-  LogTimeline({required this.entry});
+  bool done = false;
+  LogTimeline({required this.entry}) {
+    done = entry["successful"] == 1 ? true : false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 40),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
@@ -17,22 +22,45 @@ class LogTimeline extends StatelessWidget {
                 radius: 20,
                 child: Text('A'),
               ),
-              const SizedBox(width: 20,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(entry["description"], style: Theme.of(context).textTheme.headline3,),
-                  Row(
-                    children: [
-                      Text(entry["username"].toString(), style: Theme.of(context).textTheme.bodyText2,),
-                      const Text(" | "),
-                      Text("Day " + entry["day"].toString())
-                    ],
-                  ),
-                  const SizedBox(height: 5,),
-                  Text(entry["challenge_name"])
-                ],
-              )
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                entry["description"],
+                                style: Theme.of(context).textTheme.headline3,
+                              ),
+                              Text(
+                                entry["username"] +
+                                    " | Day " +
+                                    entry["day"].toString(),
+                                style: Theme.of(context).textTheme.bodyText2,
+                              ),
+                              const Text("joined Users: 2011")
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: !done
+                          ? Icon(Icons.close, color: Colors.red)
+                          : Icon(Icons.check, color: Theme.of(context).primaryColor,),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ],
